77aec067db6d4bd93d5dd5d5b5003f841849bc5d
TamaroWalter <tamarowalter@yahoo.de>
2023-09-08 13:51:36 +0200

    clean first version

---
+++ block_townsquare.php
@@ -0,0 +1,73 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     block_townsquare
+ * @category    string
+ * @copyright   2023 Tamaro Walter
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class block_townsquare extends block_base {
+    
+    /**
+     * Initialises the block.
+     *
+     * @return void
+     */
+    public function init() {
+        $this->title = get_string('pluginname', 'block_townsquare');
+    }
+
+    /**
+     * Gets the block contents.
+     *
+     * @return string The block HTML.
+     */
+    public function get_content() {
+        global $OUTPUT;
+
+        if ($this->content !== null) {
+            return $this->content;
+        }
+
+        $this->content = new stdClass();
+        $this->content->footer = '';
+
+        // Add logic here to define your template data or any other content.
+        $data = ['YOUR DATA GOES HERE'];
+
+        $this->content->text = $OUTPUT->render_from_template('block_townsquare/content', $data);
+
+        return $this->content;
+    }
+
+    /**
+     * Defines in which pages this block can be added.
+     *
+     * @return array of the pages where the block can be added.
+     */
+    public function applicable_formats() {
+        return [
+            'admin' => false,
+            'site-index' => true,
+            'course-view' => true,
+            'mod' => false,
+            'my' => true,
+        ];
+    }
+}
