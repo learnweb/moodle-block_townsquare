8c629119fffad671d566665a247cc0d5d2cc1ca9
TamaroWalter <tamarowalter@yahoo.de>
2023-11-16 13:46:23 +0100

    phpunit test for calendarevents added

--- tests/calendarevents_test.php
+++ tests/calendarevents_test.php
@@ -17,6 +17,7 @@
 
 namespace block_townsquare;
 
+use stdClass;
 use testing_data_generator;
 /**
  * Unit tests for the block_townsquare.
@@ -41,35 +42,75 @@ class calendarevents_test extends \advanced_testcase {
 
     /** @var stdClass The data that will be used for testing.
      * This Class contains the test data:
-     * - course1, course2
-     * - forum1, moodleoverflow1 (With one post each)
-     * - forum2, moodleoverflow2 (With one post each)
-     * - teacher
-     * - student1, student2
+     * - tow courses
+     * - a teacher,
+     * - a student of the course
+     * - one assign module (with a date when the assignment is due)
+     * - an activity completion from the assign module
+     * -
      */
     private $testdata;
 
-    /** @var townsquareevents Class to get townsquare events. */
-    private $townsquareevents;
-
     // Construct functions.
     public function setUp() : void {
+        $this->testdata = new stdClass();
         $this->resetAfterTest();
         $this->helper_course_set_up();
-        $this->townsquareevents = new townsquareevents();
+
     }
 
     public function tearDown() : void {
-        $this->townsquareevents = null;
         $this->testdata = null;
     }
 
     // Tests.
 
     /**
-     * Test, if calendar events are collected correctly.
+     * Test, if calendar events are sorted correctly.
      */
-    public function test_calendarevents() : void {
+    public function test_sortorder() : void {
+        // Set the teacher as the current logged in user.
+        $this->setUser($this->testdata->teacher);
+
+        // Get the current post events.
+        $townsquareevents = new townsquareevents();
+        $calendarevents = $townsquareevents->townsquare_get_calendarevents();
+        var_dump($calendarevents);
+        // Iterate trough all posts and check if the sort order is correct.
+        $timestamp = 9999999999;
+        $result = true;
+        foreach ($calendarevents as $event) {
+            if ($timestamp >= $event->timestart) {
+                $timestamp = $event->timestart;
+            } else {
+                $result = false;
+            }
+        }
+
+        $this->assertEquals(true, $result);
+    }
+
+    /**
+     * Test, if the users see only posts of their courses.
+     * @return void
+     */
+    public function test_course() : void {
+
+    }
+
+    /**
+     * Test, if a assignment is displayed correctly
+     * @return void
+     */
+    public function test_assignfilter() : void {
+
+    }
+
+    /**
+     * Test, if a activity completion is displayed correctly.
+     * @return void
+     */
+    public function test_activitycompletion() : void {
 
     }
 
@@ -82,6 +123,32 @@ class calendarevents_test extends \advanced_testcase {
      * - a student in each course
      */
     private function helper_course_set_up() : void {
+        global $DB;
+        $datagenerator = $this->getDataGenerator();
 
+        // Create a new course.
+        $this->testdata->course = $datagenerator->create_course(['enablecompletion' => 1]);
+
+        // Create a teacher and a student and enroll them in the course.
+        $this->testdata->teacher = $datagenerator->create_user();
+        $this->testdata->student = $datagenerator->create_user();
+        $datagenerator->enrol_user($this->testdata->teacher->id, $this->testdata->course->id, 'teacher');
+        $datagenerator->enrol_user($this->testdata->student->id, $this->testdata->course->id, 'student');
+
+        // Create an assign module and a activity completion.
+        // Make an assignment that is due in 1 week and will be graded in 2 weeks.
+        $time = time();
+        $this->testdata->assignment = $this->create_assignment($time, $time + 1209600, $time + 1209600);
+    }
+
+
+    private function create_assignment($allowsubmittsionsfromdate, $duedate, $gradingduedate) {
+        $assignrecord = [
+            'course'                            => $this->testdata->course->id,
+            'duedate'                           => $duedate,
+            'allowsubmissionsfromdate'          => $allowsubmittsionsfromdate,
+            'gradingduedate'                    => $gradingduedate,
+        ];
+        return $this->getDataGenerator()->create_module('assign', $assignrecord, ['completion' => 1]);
     }
 }
