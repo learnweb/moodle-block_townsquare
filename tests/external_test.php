7efc30142929c7c782ddd15f09b8386f1879f9fc
TamaroWalter <tamarowalter@yahoo.de>
2024-09-24 16:27:03 +0200

    php mess cleaning

--- tests/external_test.php
+++ tests/external_test.php
@@ -43,8 +43,15 @@ require_once($CFG->dirroot . '/webservice/tests/helpers.php');
  */
 final class external_test extends \advanced_testcase {
 
+    /** @var object That that is used for testing
+     * It contains an instance of the townsquare external class.
+     * */
+    private $testdata;
+
     public function setUp(): void {
         $this->resetAfterTest();
+        $this->testdata = new stdClass();
+        $this->testdata->external = new external();
     }
 
     /**
@@ -69,7 +76,7 @@ final class external_test extends \advanced_testcase {
         $this->assertEquals(false, $record);
 
         // Call the function to record the user settings and check, if the record is created.
-        $result = external::record_usersettings($usersetting->userid,
+        $result = $this->testdata->external->record_usersettings($usersetting->userid,
                                                                   $usersetting->timefilterpast,
                                                                   $usersetting->timefilterfuture, $usersetting->basicletter,
                                                                   $usersetting->completionletter, $usersetting->postletter);
@@ -92,7 +99,7 @@ final class external_test extends \advanced_testcase {
         $usersetting->postletter = 0;
 
         // Call the function to record the user settings and check, if the record is created.
-        $result = external::record_usersettings($usersetting->userid, $usersetting->timefilterpast,
+        $result = $this->testdata->external->record_usersettings($usersetting->userid, $usersetting->timefilterpast,
                                                                   $usersetting->timefilterfuture, $usersetting->basicletter,
                                                                   $usersetting->completionletter, $usersetting->postletter);
 
@@ -118,7 +125,7 @@ final class external_test extends \advanced_testcase {
         $this->assertEquals(1, count($DB->get_records('block_townsquare_preferences', ['userid' => 1])));
 
         // Test case 1: Wrong parameters.
-        external::reset_usersettings(2);
+        $this->testdata->external->reset_usersettings(2);
         $this->assertEquals(1, count($DB->get_records('block_townsquare_preferences', ['userid' => 1])));
 
         // Test case 2: For some reason, many records from the same user exist.
@@ -126,14 +133,14 @@ final class external_test extends \advanced_testcase {
             'timefilterfuture' => 2592000, 'basicletter' => 1, 'completionletter' => 0, 'postletter' => 0, ]);
         $this->assertEquals(2, count($DB->get_records('block_townsquare_preferences', ['userid' => 1])));
 
-        external::reset_usersettings(1);
+        $this->testdata->external->reset_usersettings(1);
         $this->assertEquals(0, count($DB->get_records('block_townsquare_preferences', ['userid' => 1])));
 
         // Test case 3: normal case.
         $DB->insert_record('block_townsquare_preferences', ['userid' => 1, 'timefilterpast' => 432000,
             'timefilterfuture' => 2592000, 'basicletter' => 0, 'completionletter' => 1, 'postletter' => 1, ]);
         $this->assertEquals(1, count($DB->get_records('block_townsquare_preferences', ['userid' => 1])));
-        external::reset_usersettings(1);
+        $this->testdata->external->reset_usersettings(1);
         $this->assertEquals(0, count($DB->get_records('block_townsquare_preferences', ['userid' => 1])));
     }
 }
